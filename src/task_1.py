5944f706d0b71c283f48d61416eaffb0b02d70be
Starkillxr <49885347+Starkillxr@users.noreply.github.com>
2023-02-28 15:32:13 +0000

    commit

--- src/task_1.py
+++ src/task_1.py
@@ -2,8 +2,23 @@ import cv2 as cv
 import numpy as np
 import os, sys
 
+
+#Variables to run the program
+#
+#Which video? 1 2 or 3
+x = 1
+
+#Selecting the video
+if(x == 1):
+  cap = cv.VideoCapture("D:/Videos/Video1 for Vision CW.avi")
+elif(x == 2):
+  cap = cv.VideoCapture("D:/Videos/Video2 for Vision CW.avi")
+elif(x == 3):
+    cap = cv.VideoCapture("D:/Videos/Video3 for Vision CW.avi")
+else:
+  print("Select Value 1, 2 or 3 for a video")
+
 cv.startWindowThread()
-cap = cv.VideoCapture("D:/Videos/Video1 for Vision CW.avi")
 
 #Params for inner blob
 params = cv.SimpleBlobDetector_Params()
@@ -44,62 +59,65 @@ params2.minInertiaRatio = 0.1
 if (cap.isOpened()== False): 
   print("Error opening video stream or file")
  
-# Read until video is completed
-while(cap.isOpened()):
-  # Capture frame-by-frame
-  ret, frame = cap.read()
-  if ret == True:
-    #Blob Detection
-    rgb = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
-    gray = cv.cvtColor(rgb, cv.COLOR_RGB2GRAY)
+for i in range(1,3):
+  # Read until video is completed
+  x=i
+  while(cap.isOpened()):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    if ret == True:
+      #Blob Detection
+      rgb = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+      gray = cv.cvtColor(rgb, cv.COLOR_RGB2GRAY)
 
-    kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (7,7))
+      kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (7,7))
 
-    gray = cv.dilate(gray, kernel, iterations = 1)
-    gray = cv.erode(gray, kernel, iterations = 1)
+      gray = cv.dilate(gray, kernel, iterations = 1)
+      gray = cv.erode(gray, kernel, iterations = 1)
 
-    hsv = cv.cvtColor(rgb, cv.COLOR_RGB2HSV)
-    #Hue
-    lowH = 0
-    highH = 180
+      hsv = cv.cvtColor(rgb, cv.COLOR_RGB2HSV)
+      #Hue
+      lowH = 0
+      highH = 180
 
-    #Saturation
-    lowS = 0
-    highS = 255
+      #Saturation
+      lowS = 0
+      highS = 255
 
-    #Value
-    lowV = 0
-    highV = 70
+      #Value
+      lowV = 0
+      highV = 70
 
-    thresholdIMG = cv.inRange(hsv, (lowH, lowS, lowV), (highH, highS, highV))
+      thresholdIMG = cv.inRange(hsv, (lowH, lowS, lowV), (highH, highS, highV))
 
-    kernel2 = cv.getStructuringElement(cv.MORPH_ELLIPSE,(3,3))
-    cannyThreshold = 55
-    thresholdIMG = cv.dilate(thresholdIMG, kernel2, iterations = 2)
-    thresholdIMG = cv.erode(thresholdIMG, kernel2, iterations = 1)
-    thresholdIMG = cv.Canny(thresholdIMG, cannyThreshold, cannyThreshold*2)
+      kernel2 = cv.getStructuringElement(cv.MORPH_ELLIPSE,(3,3))
+      cannyThreshold = 55
+      thresholdIMG = cv.dilate(thresholdIMG, kernel2, iterations = 2)
+      thresholdIMG = cv.erode(thresholdIMG, kernel2, iterations = 1)
+      thresholdIMG = cv.Canny(thresholdIMG, cannyThreshold, cannyThreshold*2)
 
-    detector = cv.SimpleBlobDetector_create(params)
-    detector2 = cv.SimpleBlobDetector_create(params2)
-    blobs = detector.detect(gray)
-    blobs2 = detector2.detect(thresholdIMG)
+      detector = cv.SimpleBlobDetector_create(params)
+      detector2 = cv.SimpleBlobDetector_create(params2)
+      blobs = detector.detect(gray)
+      blobs2 = detector2.detect(thresholdIMG)
 
-    inputImgBlobs = cv.drawKeypoints(rgb, blobs, np.array([]), (0,0,255), cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-    inputImgBlobs = cv.drawKeypoints(inputImgBlobs, blobs2, np.array([]), (0,255,0), cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-    # Display the resulting frame
-    cv.imshow('RGB', rgb)
-    cv.imshow('Gray', gray)
-    cv.imshow('HSV', hsv)
-    cv.imshow('ThresholdIMG', thresholdIMG)
-    cv.imshow('Inner & Outer Blobs', inputImgBlobs)
- 
-    # Press Q on keyboard to  exit
-    if cv.waitKey(50) & 0xFF == ord('q'):
+      inputImgBlobs = cv.drawKeypoints(rgb, blobs, np.array([]), (0,0,255), cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+      inputImgBlobs = cv.drawKeypoints(inputImgBlobs, blobs2, np.array([]), (0,255,0), cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+      # Display the resulting frame
+      cv.imshow('RGB', rgb)
+      cv.imshow('Gray', gray)
+      cv.imshow('HSV', hsv)
+      cv.imshow('ThresholdIMG', thresholdIMG)
+      cv.imshow('Inner & Outer Blobs', inputImgBlobs)
+
+      # Press Q on keyboard to  exit
+      if cv.waitKey(50) & 0xFF == ord('q'):
+        break
+    # Break the loop
+    else: 
       break
-  # Break the loop
-  else: 
+  if cv.waitKey(5) & 0xFF == ord('q'):
     break
- 
 # When everything done, release the video capture object
 cap.release()
  
