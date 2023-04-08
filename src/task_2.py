6a48beed1afb08a806ee4c7d6fad3af4ef230543
starkillxr <49885347+Starkillxr@users.noreply.github.com>
2023-04-08 13:24:07 +0100

    updated task 2

--- src/task_2.py
+++ src/task_2.py
@@ -85,7 +85,7 @@ for epoch in range(100):  # loop over the dataset multiple times
 
         # print statistics
         running_loss += loss.item()
-        if i % 200 == 199:    # print every 2000 mini-batches
+        if i % 2 == 1:    # print every 2000 mini-batches
             print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 200:.3f}')
             running_loss = 0.0
 
